045fac3faa44f988ba58ab0c54a654a8c41f4ac8
amithapase <amithapase@outlook.com>
2023-09-18 22:43:23 +0530

    set video bitrate

--- ytdlp.py
+++ ytdlp.py
@@ -1,14 +1,16 @@
+"""
+Youtube video downloader program.
+"""
 import os
 
 import yt_dlp
 
-# test comment
 
-
-def get_video_formats(video_url):
+def get_video_formats(url):
+    """Getting video formats"""
     ydl = yt_dlp.YoutubeDL()
 
-    info = ydl.extract_info(video_url, download=False)
+    info = ydl.extract_info(url, download=False)
 
     formats = info["formats"]
 
@@ -25,14 +27,13 @@ def get_video_formats(video_url):
     if format_choice.isnumeric() and 1 <= int(format_choice) <= len(formats):
         chosen_format = formats[int(format_choice) - 1]
         return chosen_format
-    else:
-        print("Invalid format choice.")
-        return None
+    print("Invalid format choice.")
+    return None
 
 
-def download_youtube_video(video_url):
-    # Get chosen format
-    chosen_format = get_video_formats(video_url)
+def download_youtube_video(url):
+    """Downloading video."""
+    chosen_format = get_video_formats(url)
 
     if chosen_format is not None:
         # Create YT-DLP downloader object
@@ -41,18 +42,21 @@ def download_youtube_video(video_url):
         # Set download options
         video_options = {
             "format": chosen_format["format_id"],
-            "outtmpl": "~/storage/download/%(title)s.%(ext)s",  # Output filename template
+            "keepvideo": True,
+            # Output filename template
+            "outtmpl": "~/storage/download/%(title)s.%(ext)s",
         }
 
         # Download the YouTube video
         with yt_dlp.YoutubeDL(video_options) as ydl:
-            video_info = ydl.extract_info(video_url, download=True)
+            video_info = ydl.extract_info(url, download=True)
             video_filename = ydl.prepare_filename(video_info)
 
         # Set download options for audio
         audio_options = {
             "format": "bestaudio/best",
-            "outtmpl": "~/storage/download/%(title)s.%(ext)s",  # Output audio filename template
+            # Output audio filename template
+            "outtmpl": "~/storage/download/%(title)s.%(ext)s",
             "postprocessors": [
                 {
                     "key": "FFmpegExtractAudio",
@@ -64,12 +68,13 @@ def download_youtube_video(video_url):
 
         # Download the YouTube audio
         with yt_dlp.YoutubeDL(audio_options) as ydl:
-            audio_info = ydl.extract_info(video_url, download=True)
+            audio_info = ydl.extract_info(url, download=True)
             audio_filename = ydl.prepare_filename(audio_info)
 
         # Merge video and audio using FFmpeg
         merged_filename = f"{video_filename[:-4]}_merged.mp4"
-        merge_command = f'ffmpeg -i "{video_filename}" -i "{audio_filename}" -c:v copy -c:a copy "{merged_filename}"'
+        video_bitrate = "2000k"
+        merge_command = f'ffmpeg -i "{video_filename}" -i "{audio_filename}" -c:v copy -b:v {video_bitrate} -c:a copy "{merged_filename}"'
         os.system(merge_command)
 
         print("Video and audio merged successfully!")
